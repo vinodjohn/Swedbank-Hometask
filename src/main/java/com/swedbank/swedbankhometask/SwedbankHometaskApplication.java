f1188ab5aa68d3109536875e2ca9922065d75160
vinodjohn <vinodjohn56@gmail.com>
2026-07-22 20:14:36 +0300

    Initial commit

---
+++ src/main/java/com/swedbank/swedbankhometask/SwedbankHometaskApplication.java
@@ -0,0 +1,13 @@
+package com.swedbank.swedbankhometask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwedbankHometaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwedbankHometaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swedbank/swedbankhometask/SwedbankHometaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swedbank.swedbankhometask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwedbankHometaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
